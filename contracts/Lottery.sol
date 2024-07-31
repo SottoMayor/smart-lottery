7722809d6a28544b397cd01b8dd16a6413527d59
David Sotto Mayor <ddsmmf@gmail.com>
2024-07-31 11:56:11 -0400

    Defining players and building the enter function

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,8 +2,15 @@ pragma solidity ^0.4.17;
 
 contract Lottery {
     address public manager;
+    address[] public players;
 
     function Lottery() {
         manager = msg.sender;
+    }
+
+    function enter() public payable {
+        // OBS: Prevent the manager to enter.
+
+        players.push(msg.sender);
     }
 }

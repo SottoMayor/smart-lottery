92f50fcc4c8918e121acdde80b5165cb6475851d
David Sotto Mayor <ddsmmf@gmail.com>
2024-08-01 12:36:56 -0400

    Configuring the test suuite

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,3 +1,19 @@
 const ganache = require('ganache');
 const { Web3 } = require('web3');
-// updated imports added for convenience
+const assert = require('assert');
+const { interface, bytecode } = require('../compile');
+
+const web3 = new Web3(ganache.provider());
+
+let accounts;
+let lottery;
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+
+    lottery = await new web3
+    .eth
+    .Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({from: accounts[0], gas: '1000000'})
+})

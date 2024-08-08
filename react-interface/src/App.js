24d5e8562667c8bc2700d116cb3b6539f3562898
David Sotto Mayor <ddsmmf@gmail.com>
2024-08-08 10:46:19 -0400

    Adding a form to enter ether

--- react-interface/src/App.js
+++ react-interface/src/App.js
@@ -1,15 +1,23 @@
 import "./App.css";
 import React from "react";
 import lottery from "./lottery";
+import web3 from "./web3";
 
 class App extends React.Component {
 
-  state = {manager: ''};
+  state = {
+    manager: '',
+    players: [],
+    balance: '',
+    value: ''
+  };
 
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
 
-    this.setState({ manager })
+    this.setState({ manager, players, balance })
   }
 
   render() {
@@ -17,6 +25,27 @@ class App extends React.Component {
       <div>
         <h2>Smart Lottery</h2>
         <p>Manager: {this.state.manager}</p>
+        <p>
+          There are currently {this.state.players.length} people entered, competiting to win {' '} 
+          {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+        </p>
+
+        <hr/>
+
+        <form>
+
+          <h4>Want to try your luck?</h4>
+
+          <div>
+            <label>Amount of Ether to enter:</label>
+            <input
+             type="text" value={this.state.value} 
+             onChange={event => this.setState({value: event.target.value})}
+            />
+          </div>
+
+          <button>Enter!</button>
+        </form>
       </div>
     );
   }
